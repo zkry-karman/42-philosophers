46545af91408c066d8fe9e4d1e55917872ae19ee
Zack Karman <zkry.karman@gmail.com>
2026-03-17 13:48:25 +0100

    redoing setup

--- main.c
+++ main.c
@@ -3,59 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zkarman <zkarman@student.42.fr>            +#+  +:+       +#+        */
+/*   By: karmanz <karmanz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 14:02:08 by zkarman           #+#    #+#             */
-/*   Updated: 2026/03/15 20:31:11 by zkarman          ###   ########.fr       */
+/*   Updated: 2026/03/17 13:48:06 by karmanz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void    ft_makenode(t_philo *philosophers, int i)
+t_data  initialize_data(char **args)
 {
-    t_philo *newnode;
-    t_philo *lastnode;
-
-    newnode = malloc(sizeof(t_philo));
-    if (!newnode)
-        return ;
-    newnode->
-}
-
-t_philo *a_table(int ac, char **av)
-{
-    int i;
-    int number_of_philosphers;
-    t_philo *philosophers; 
-
-    i = 1;
-    number_of_philosphers = av[1];
-    while (i < number_of_philosphers)
-    {
-        ft_makenode(philosophers, i);
-        i++;
-    }
+    t_data  *data;
+    
 }
 
 int valid_arguments(int ac, char **av)
 {
     if (ac < 5 || ac > 6)
         return(printf("%s\n", "Must have either 4 or 5 arguments"), 0);
+    if (av[1] < 1)
+        return(printf("%s\n", "Need at least 1 philosopher"), 0);
     if (av[1] > INT_MAX || av[2] > INT_MAX || av[3] > INT_MAX || av[4] > INT_MAX)
         return(printf("%s%d\n", "Parameters must be less than or equal to: " INT_MAX), 0);
-    if (av[1] < INT_MIN || av[2] < INT_MIN || av[3] < INT_MIN || av[4] < INT_MIN)
-        return(printf("%s%d\n", "Parameters must be greater than or equal to: " INT_MIN), 0);
-    if(av[5] && (av[5] < INT_MIN || av[5] > INT_MAX))
-        return(printf("%s%d%s%d\n" "Parameters must be between " INT_MIN, "and " INT_MAX), 0);
+    if (av[2] < 0 || av[3] < 0 || av[4] < 0)
+        return(printf("%s%d\n", "Parameters must be greater than or equal to: " 0), 0);
+    if(av[5] && (av[5] < 0 || av[5] > INT_MAX))
+        return(printf("%s%d%s%d\n" "Parameters must be between " 0, "and " INT_MAX), 0);
     return (1);
 }
 
 int main(int ac, char **av)
 {
-    t_philo *philos;
-
+    t_data  *data;
+    
     if (!valid_arguments(ac, av))
         return (1);
-    philos = a_table(ac, av);
+    data = initialize_data(av);
 }
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: karmanz <karmanz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 14:04:50 by karmanz           #+#    #+#             */
-/*   Updated: 2026/03/16 14:08:29 by karmanz          ###   ########.fr       */
+/*   Updated: 2026/03/17 13:47:15 by karmanz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,22 @@
 # define PHILO_H
 #include <stdio.h>
 #include <limits.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-typedef struct s_philo
+typedef struct s_data
 {
+    int number_of_philosophers;
     int time_to_die;
     int time_to_eat;
     int time_to_sleep;
     int number_of_times_each_philosopher_must_eat;
-}
+
+    long long   start_time;
+    int is_dead;
+    pthread_mutex_t write_lock;
+    pthread_mutex_t dead_lock;
+}   t_data;
 
 int main(int ac, char **av)
 
