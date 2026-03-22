c39445eab174d6dff53578e063476d53bfb31f8c
Zack Karman <zkry.karman@gmail.com>
2026-03-22 23:28:53 +0100

    added reminder to add timestamps in is_eating

--- simulation/eating.c
+++ simulation/eating.c
@@ -6,7 +6,7 @@
 /*   By: karmanz <karmanz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/22 21:13:29 by karmanz           #+#    #+#             */
-/*   Updated: 2026/03/22 22:47:29 by karmanz          ###   ########.fr       */
+/*   Updated: 2026/03/22 23:28:21 by karmanz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void    is_eating(t_philo *philo)
 {
+    //Need to add timestamps for all the printfs
     pthread_mutex_lock(philo->l_fork);
     pthread_mutex_lock(philo->data.write_lock);
     printf("%d has taken a fork", philo->id);
