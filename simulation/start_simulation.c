dd877d7dd096d54008af8a37ebf13bbfb370202e
Zack Karman <zkry.karman@gmail.com>
2026-04-26 23:15:46 +0200

    adding philo monitor function

--- simulation/start_simulation.c
+++ simulation/start_simulation.c
@@ -6,11 +6,16 @@
 /*   By: karmanz <karmanz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/21 15:52:04 by zkarman           #+#    #+#             */
-/*   Updated: 2026/04/26 22:05:17 by karmanz          ###   ########.fr       */
+/*   Updated: 2026/04/26 23:14:47 by karmanz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void    *check_for_deaths(void *arg)
+{
+    
+}
 
 void    *philosopher_routine(void *arg)
 {
@@ -28,14 +33,13 @@ void    *philosopher_routine(void *arg)
             break ;
         is_eating(philo);
         is_sleeping(philo);
-        //need to create is_thinking
         is_thinking(philo);
     }
-    return (NULL);
 }
 
 void    start_simulation(t_philo *philos, t_data *data)
 {
+    pthread_t   death_check;
     int i;
     
     data->start_time = get_time();
@@ -45,6 +49,7 @@ void    start_simulation(t_philo *philos, t_data *data)
         pthread_create(&philos[i].thread, NULL, &philosopher_routine, &philos[i]);
         i++;
     }
+    pthread_create(&death_check, NULL, &check_for_deaths, NULL)
     i = 0;
     while (i < data->number_of_philosophers)
     {
