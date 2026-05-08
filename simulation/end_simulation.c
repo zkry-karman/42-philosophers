a6a1e185da8d452388b02b3ae8bb8366dd3a6ba8
zkarman <zkarman@f6r3s13.paris.42.school>
2026-05-08 14:32:42 +0200

    freed forks

--- simulation/end_simulation.c
+++ simulation/end_simulation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   end_simulation.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: karmanz <karmanz@student.42.fr>            +#+  +:+       +#+        */
+/*   By: zkarman <zkarman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 15:43:16 by karmanz           #+#    #+#             */
-/*   Updated: 2026/05/06 15:37:27 by zkarman          ###   ########.fr       */
+/*   Updated: 2026/05/08 14:27:19 by zkarman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	end_simulation(t_philo *philos, t_data *data)
 {
 	if (philos)
 		free (philos);
+	if (data->forks)
+		free (data->forks);
 	if (data)
 		free (data);
 }
